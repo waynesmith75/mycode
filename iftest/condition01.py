921d895e606317925eb0e436ad581b935de90faf
Wayne Smith <wayne.smith2@gmail.com>
2020-03-03 12:40:00 -0500

    testing if conditions

---
+++ iftest/condition01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#create the string hostname
+hostname = "MTG"
+# test logic with the "if" statement
+# what to do if this statement is found to be true
+if hostname == "MTG":
+    print("This hostname was found to be mtg")
+
+
---
+++ iftest/condition03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#create the string hostname
+hostname = input("What value should we set for hostname?")
+# test logic with the "if" statement
+# what to do if this statement is found to be true
+if hostname.lower() == "mtg":
+    print("This hostname was found to be mtg")
+
+
