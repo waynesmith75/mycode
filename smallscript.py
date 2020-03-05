a5777aed0b873a74f20a1a664624a4c9c1f2ee76
Wayne Smith <wayne.smith2@gmail.com>
2020-03-05 09:18:21 -0500

    best practices and pylint

---
+++ smallscript.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env pythong3
+
+"""
+Author: RZFeeser
+
+This program prints some things to the screen
+"""
+
+def main():
+    """Our main program"""
+    mynote = "print this string" #define mynote
+    print(mynote)    #display our string
+
+main()
