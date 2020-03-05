37b83406eb02c16d405163818ad232143b15160d
Wayne Smith <wayne.smith2@gmail.com>
2020-03-05 15:37:39 -0500

    python APIs and JSON

---
+++ catfacts/facts01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import requests
+
+def main():
+    r = requests.get("https://cat-fact.herokuapp.com/facts")
+
+    print(r.json())
+main()
---
+++ catfacts/facts03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import requests
+
+def main():
+    r = requests.get("https://cat-fact.herokuapp.com/facts")
+    for catfact in r.json()["all"]:
+        print(catfact.get("text"))
+main()
